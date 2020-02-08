afc5015dc30b4524de4d028ccd2fe5a140103be2
github username <github email address>
2020-02-08 15:17:26 -0500

    test2

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
 function largestSubarraySum(array){
     // code to write here
+    let newArray = []
+    for (let i = 0; i < array.length; ++i){
+        array[i]
+    }
    }
+
+
+
+// <button onclick="myFunction()">Try it</button>
+
+// <p>Sum of numbers in array: <span id="demo"></span></p>
+
+// <script>
+// var numbers = [15.5, 2.3, 1.1, 4.7];
+
+// function getSum(total, num) {
+//   return total + Math.round(num);
+// }
+
+// function myFunction(item) {
+//   document.getElementById("demo").innerHTML = numbers.reduce(getSum, 0);
+// }
+// </script>
+
+
+
+
+
+
+
+
+
+
+//    function longestConsec(strarr, k){
+//     var longest = "";
+//     for(var i=0;k>0 && i<=strarr.length-k;i++){
+//       var tempArray = strarr.slice(i,i+k);
+//       var tempStr = tempArray.join("");
+//       if(tempStr.length > longest.length){
+//         longest = tempStr;
+//       }
+//     }
+//     return longest;
+// }
